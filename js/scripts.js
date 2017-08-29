209975d5fa143c5dfb96fa06732c7448ad073022
Agata <agatakad@gmail.com>
2017-08-29 15:51:02 +0200

    Dodanie średnika do lini 7

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,6 @@ var a = 8;
 var h = 4;
 var triangleArea;
 
-triangleArea = a * h / 2
+triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 alert('Pole trójkąta o wysokości ' + h + ' i podstawie wynoszącej ' + a + ' jest równe ' + triangleArea);
